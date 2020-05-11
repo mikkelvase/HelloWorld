cf20637960d66099c336e0ee1fbe94f15a03f15f
mikkelvase <Mikk864n@stud.kea.dkgit config --global user.email mikk864n@stud.eka.dk>
2020-05-11 14:52:59 +0200

    First Commit, printing Hello World

---
+++ src/com/Mikkel/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.Mikkel;
+
+
+
+public class HelloWorld {
+
+    public void printStatement(String text) {
+
+        System.out.println(text);
+    }
+
+}
---
+++ src/com/Mikkel/Main.java
@@ -0,0 +1,11 @@
+package com.Mikkel;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HelloWorld hi = new HelloWorld();
+
+        hi.printStatement("Hello World");
+    }
+}
